5412c1c3b565446e33178a3a94019cea966ca669
ShaoEnChen <seanchen47@gmail.com>
2018-08-31 06:43:18 -0500

    add  prototype for Vue components

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import 'babel-polyfill';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import axios from 'axios';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+Vue.prototype.$http = axios;
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
