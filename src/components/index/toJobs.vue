3fce89a77cc4b26d10886d161d11aab1a7e02c3b
ShaoEnChen <seanchen47@gmail.com>
2018-09-03 11:52:25 -0500

    add uncertain blocks

--- src/components/index/toJobs.vue
+++ src/components/index/toJobs.vue
@@ -1,6 +1,12 @@
 <template>
 	<b-container fluid id="to-jobs">
-
+		<b-row>
+			<b-col>
+				<h1 class="title mt-2 py-5">
+					職缺一覽
+				</h1>
+			</b-col>
+		</b-row>
 	</b-container>
 </template>
 
@@ -12,4 +18,11 @@ export default {
 
 <style lang="sass" scoped>
 	@import '@/assets/css/global.scss'
+
+	#to-jobs
+		background-color: $black
+
+		.title
+			color: $white
+			text-align: center
 </style>
--- src/components/index/toOB.vue
+++ src/components/index/toOB.vue
@@ -1,6 +1,12 @@
 <template>
 	<b-container fluid id="to-ob">
-
+		<b-row>
+			<b-col>
+				<h1 class="title mt-2 py-5">
+					OB 分享
+				</h1>
+			</b-col>
+		</b-row>
 	</b-container>
 </template>
 
@@ -12,4 +18,11 @@ export default {
 
 <style lang="sass" scoped>
 	@import '@/assets/css/global.scss'
+
+	#to-ob
+		background-color: $white
+
+		.title
+			color: $dark-green
+			text-align: center
 </style>
