4c9623ad0ad131e655e833cda8c90989fcd8f2d6
ShaoEnChen <seanchen47@gmail.com>
2018-09-07 21:20:00 -0500

    make main.js more clear

--- src/main.js
+++ src/main.js
@@ -9,8 +9,9 @@ import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.prototype.$http = axios;
 Vue.use(BootstrapVue);
+
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false;
 
