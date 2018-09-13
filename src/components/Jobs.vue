c44785775c549f185570c7be55937b87ed4d1f62
ShaoEnChen <seanchen47@gmail.com>
2018-09-13 00:57:10 -0500

    add nav, remove data

--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -1,308 +1,301 @@
 <template>
-	<b-container fluid id="jobs">
-		<div class="title">
-			<h2>{{ msg }}</h2>
-			<h5>若對我們職缺有興趣，請點選“立即加入國泰”！</h5>
-		</div>
-		<div class="job">
-			<h4>客服系統整合工程師</h4>
-			<h5>國泰人壽 / 國泰產險</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>客服中心(Contact Center)資訊系統及電話語音系統之規劃、開發與維運。</li>
-					<li> 客服資通訊科技之應用研究及導入評估。<br></li>
-				</ul>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>具IP網路、語音通訊、客戶服務相關系統開發經驗者尤佳。</li>
-					<li> 無相關經驗但有意願學習客服系統(含交換機,IVR)相關知識技能者亦可。<br></li>
-					<li> 具Java EE、HTML、JavaScript、Java等開發經驗者尤佳。</li>
-				</ul>
+	<b-container fluid>
+		<Nav></Nav>
+		<b-container fluid id="jobs">
+			<div class="title">
+				<h2>所有職缺</h2>
+				<h5>若對我們職缺有興趣，請點選“立即加入國泰”！</h5>
 			</div>
-		</div>
-		<div class="job">
-			<h4>應用系統開發工程師</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>國壽人壽審查契約、理賠、保單變更、人事管理、財務投資系統等線上系統建置，以支持壽險公司營運。</li>
-					<li>國泰人壽官網(B2C)、員工網站(B2E)、保險電子商務系統、客戶關係管理系統、行銷支援系統及資訊業務部門各項專案系統之建置、開發及維護。</li>
-					<li>iOS／Android App相關專案之系統開發及維護。</li>
-				</ul>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>熱愛程式開發，無資訊背景亦可。</li>
-					<li>具備Java EE、HTML、JavaScript等開發經驗者，已考取Java專業證照者佳。</li>
-					<li>有iOS / Android App開發、測試、上架、性能優化經驗者，附個人相關作品者尤佳。</li>
-					<li>熟悉前端UI框架運用或具RWD開發經驗尤佳。</li>
-					<li>為儲備行動化App設計人員，具備Objective C、HTML5、CSS3開發經驗者尤佳。</li>
-				</ul>
-
-			</div>
-		</div>
-		<div class="job">
-			<h4>應用系統分析工程師</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>國壽人壽審查契約、理賠、保單變更、人事管理、投資型商品、團險、意外險、財務投資等線上系統規劃，以支持壽險公司營運，除了系統分與維護，更能了解壽險公司整體經營之狀況</li>
-					<li>國泰人壽官網(B2C)、員工網站(B2E)、保險電子商務、客戶關係管理及行銷支援等線上系統之分析規劃，以提供壽險公司內外部客戶行銷、服務及電子商務之最佳化平台。</li>
-				</ul>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>對壽險系統維護俱有1年以上相關經驗。</li>
-					<li>有參與設計過壽險系統專案、電子商務系統維護或具備Java、Objective C程式開發經驗者尤佳。</li>
-				</ul>
-			</div>
-		</div>
-		<div class="job">
-			<h4>伺服器系統管理工程師(Linux、Windows、AIX)</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>規劃、維運Linux Server平台，及相關軟硬體建置、維護與監控。</li>
-					<li>資訊系統專案架構之規劃、評估與建置。</li>
-					<li>評估、導入開源技術或新資訊科技應用。</li>
-				</ul>
-				<br>
-				歡迎具下列任一領域經驗、專長或證照者：<br>
-				<ul>
-					<li>Unix-like系統相關管理、Java Based應用伺服器管理。</li>
-					<li>負載平衡管理、儲存及備份設備等基本概念。</li>
-					<li>Java、Web Application、網路及通訊等基本概念。</li>
-					<li>虛擬化平台 (VMware vSphere／Microsoft Hyper-V／Power VM) 等基本概念。</li>
-					<li>大型架構(超過100台伺服器)系統維運經驗者。</li>
-				</ul>
-			</div>
-		</div>
-		<div class="job">
-			<h4>應用伺服器系統工程師(WAS、JBoss)</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>負責應用伺服器系統(IBM WAS、Red Hat JBoss)管理。</li>
-					<li> 電腦作業系統(Linux、Windows 或AIX)管理。</li>
-				</ul>
-				<br>
-				歡迎具下列任一領域經驗、專長或證照者：<br>
-				<ul>
-					<li>Java Based應用伺服器管理。</li>
-					<li>Linux、Windows 或AIX管理。</li>
-					<li>大型架構(超過100台伺服器)系統維運經驗者。</li>
-					<li>專案團隊經驗者或小型工作團隊經驗者。</li>
-				</ul>
-			</div>
-		</div>
-
-		<div class="job">
-			<h4>資料庫系統管理工程師(DB2、SQL、Open source)</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>資料庫及相關軟硬體建置、維護與監控。</li>
-					<li>資料庫效能分析與優化調校。</li>
-					<li>資訊系統專案架構之規劃、評估與建置。</li>
-				</ul>
-				<br>
-				歡迎具下列任一領域經驗、專長或證照者報名：<br>
-				<ul>
-					<li>AIX、Linux系統相關管理。</li>
-					<li>DB2或SQL Server系統管理。</li>
-					<li>大型資料庫管理及設計經驗者。</li>
-				</ul>
-			</div>
-		</div>
-
-		<div class="job">
-			<h4>雲端虛擬化系統工程師(VMWare、Hyper-V)</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>規劃、維運雲端虛擬化技術平台，並協助轉移現有系統至雲端平台。</li>
-					<li>評估、導入雲端、開源技術或新資訊科技應用。</li>
-				</ul>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>具VMware、Hyper-V虛擬化技術、Windows、Linux伺服器作業系統或Jboss應用伺服器等任一技術建置、維運有經驗者尤佳，有意願者亦可。</li>
-					<li>對雲端、開源技術或新資訊科技應用評估、規劃與發展有意願者。</li>
-				</ul>
-			</div>
-		</div>
-
-		<div class="job">
-			<h4>網路系統管理工程師</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>規劃、維運企業網路系統、無線網路、防火牆之建置、維護與監控。</li>
-					<li>網路系統專案架構之規劃、評估與建置。</li>
-					<li>評估、導入網路安全技術或網路新科技應用。</li>
-				</ul>
-				<br>
-				歡迎具下列任一領域經驗、專長或證照者：<br>
-				<ul>
-					<li>熟悉TCP/IP網路架構及規劃(Router/Switch/Wireless)。</li>
-					<li>CCNA證照。</li>
-					<li>Windows、UNIX、Linux等作業系統經驗者。</li>
-					<li>專案團隊經驗者或小型工作團隊經驗者。</li>
-				</ul>
-			</div>
-		</div>
-
-		<div class="job">
-			<h4>應用系統品質管理工程師</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				負責應用系統迴歸架構、測試自動化及效能監測之規劃、開發與維護。<br>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>歡迎無資訊背景但對軟體測試及程式開發有興趣者。</li>
-					<li>具HTML、JavaScript、Java EE開發經驗者或軟體開發測試證照尤佳。</li>
-				</ul>
-			</div>
-		</div>
-
-		<div class="job">
-			<h4>大數據資料工程師</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>因應金融業務場景，開發大數據資料蒐集、清洗及邏輯處</li>
-					<li>規劃大數據資料倉儲與應用平台(如：機器學習平台、商業智慧工具)架構。</li>
-				</ul>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>大學日間部以上，數理統計、資工資管等相關等系所畢。</li
-					<li>熟悉資料結構與關聯式資料庫存取語言SQL並具備ETL相關技術。</li>
-					<li>熟悉Hadoop架構與Spark。</li>
-					<li>具備程式撰寫能力(R或Python)。</li>
-					<li>具大數據機器學習相關開發框架經驗者尤佳，如：Caffe、Theano、TensorFlow、Torch。</li>
-				</ul>
-			</div>
-		</div>
-
-		<div class="job">
-			<h4>前端工程師 (Front-end Developer)</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>與UX/UI及開發人員共同合作，規劃與設計網路投保、會員網站及行銷類網站的</li>
-					<li>網頁程式開發。</li>
-					<li>串接Ajax、Restful API。</li>
-					<li>前端技術研究。</li>
-				</ul>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>對前端開發</li>
-					<li>善於溝通、對改善產品使用者經驗有強烈的興趣與企圖心。</li>
-					<li>熟悉JavaScript、CSS等開發，有相關工作經驗者尤佳。</li>
-					<li>熟悉任一前端框架(Angular, React, Vue)、具RWD網站開發或各式版型模組化能力、具UX/UI經驗、具後端經驗者尤佳。</li>
-				</ul>
-			</div>
-		</div>
-
-		<div class="job">
-			<h4>智能客服研發工程師</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>運用語音辨識，語意理解等技術於對話機器人，知識庫等客服應用系統之規劃、開發</li>
-					<li>客服資通訊科技之應用研究及導入評估。</li>
-				</ul>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>具Java EE、JavaScript、Node-RED等開發經驗</li>
-					<li>具ChatBot，語音辨識應用之經驗者尤佳。</li>
-				</ul>
-			</div>
-		</div>
-
-		<div class="job">
-			<h4>iOS/Android App 開發工程師</h4>
-			<h5>國泰人壽</h5>
-			<div class="btn">立即填寫履歷</div>
-			<div class="line"></div>
-			<div class="content">
-				工作說明：<br>
-				<ul>
-					<li>iOS／Android App相關專案之系統開發</li>
-					<li>加入App敏捷團隊，與 UX/UI 及後端開發人員共同合作。</li>
-				</ul>
-				<br>
-				職位要求：<br>
-				<ul>
-					<li>熱愛程式開發，無資訊背</li>
-					<li> 善於溝通、對改善產品使用者經驗有強烈的興趣與企圖心。</li>
-					<li>有 iOS/Android App 開發、測試、上架、性能優化經驗者尤佳，附個人相關作品。</li>
-					<li>有使用 Git 軟體開發經驗。</li>
-					<li>熟悉RESTful規範。</li>
-					<li具下列任一項目者尤佳：</li>
+			<div class="job">
+				<h4>客服系統整合工程師</h4>
+				<h5>國泰人壽 / 國泰產險</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
 					<ul>
-						<li>iOS以Swift/Objective-C為主要開發語言，熟CocoPods/Carthage。</li>
-						<li>Android 熟 Kotlin。</li>
-						<li>Design pattern/MVC/Git flow。 </li>
-						<li>Jenkins軟體開發工具經驗。</li>
-						<li>Agile團隊經驗。</li>
+						<li>客服中心(Contact Center)資訊系統及電話語音系統之規劃、開發與維運。</li>
+						<li> 客服資通訊科技之應用研究及導入評估。<br></li>
 					</ul>
-				</ul>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>具IP網路、語音通訊、客戶服務相關系統開發經驗者尤佳。</li>
+						<li> 無相關經驗但有意願學習客服系統(含交換機,IVR)相關知識技能者亦可。<br></li>
+						<li> 具Java EE、HTML、JavaScript、Java等開發經驗者尤佳。</li>
+					</ul>
+				</div>
 			</div>
-		</div>
+			<div class="job">
+				<h4>應用系統開發工程師</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>國壽人壽審查契約、理賠、保單變更、人事管理、財務投資系統等線上系統建置，以支持壽險公司營運。</li>
+						<li>國泰人壽官網(B2C)、員工網站(B2E)、保險電子商務系統、客戶關係管理系統、行銷支援系統及資訊業務部門各項專案系統之建置、開發及維護。</li>
+						<li>iOS／Android App相關專案之系統開發及維護。</li>
+					</ul>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>熱愛程式開發，無資訊背景亦可。</li>
+						<li>具備Java EE、HTML、JavaScript等開發經驗者，已考取Java專業證照者佳。</li>
+						<li>有iOS / Android App開發、測試、上架、性能優化經驗者，附個人相關作品者尤佳。</li>
+						<li>熟悉前端UI框架運用或具RWD開發經驗尤佳。</li>
+						<li>為儲備行動化App設計人員，具備Objective C、HTML5、CSS3開發經驗者尤佳。</li>
+					</ul>
 
+				</div>
+			</div>
+			<div class="job">
+				<h4>應用系統分析工程師</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>國壽人壽審查契約、理賠、保單變更、人事管理、投資型商品、團險、意外險、財務投資等線上系統規劃，以支持壽險公司營運，除了系統分與維護，更能了解壽險公司整體經營之狀況</li>
+						<li>國泰人壽官網(B2C)、員工網站(B2E)、保險電子商務、客戶關係管理及行銷支援等線上系統之分析規劃，以提供壽險公司內外部客戶行銷、服務及電子商務之最佳化平台。</li>
+					</ul>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>對壽險系統維護俱有1年以上相關經驗。</li>
+						<li>有參與設計過壽險系統專案、電子商務系統維護或具備Java、Objective C程式開發經驗者尤佳。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>伺服器系統管理工程師(Linux、Windows、AIX)</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>規劃、維運Linux Server平台，及相關軟硬體建置、維護與監控。</li>
+						<li>資訊系統專案架構之規劃、評估與建置。</li>
+						<li>評估、導入開源技術或新資訊科技應用。</li>
+					</ul>
+					<br>
+					歡迎具下列任一領域經驗、專長或證照者：<br>
+					<ul>
+						<li>Unix-like系統相關管理、Java Based應用伺服器管理。</li>
+						<li>負載平衡管理、儲存及備份設備等基本概念。</li>
+						<li>Java、Web Application、網路及通訊等基本概念。</li>
+						<li>虛擬化平台 (VMware vSphere／Microsoft Hyper-V／Power VM) 等基本概念。</li>
+						<li>大型架構(超過100台伺服器)系統維運經驗者。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>應用伺服器系統工程師(WAS、JBoss)</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>負責應用伺服器系統(IBM WAS、Red Hat JBoss)管理。</li>
+						<li> 電腦作業系統(Linux、Windows 或AIX)管理。</li>
+					</ul>
+					<br>
+					歡迎具下列任一領域經驗、專長或證照者：<br>
+					<ul>
+						<li>Java Based應用伺服器管理。</li>
+						<li>Linux、Windows 或AIX管理。</li>
+						<li>大型架構(超過100台伺服器)系統維運經驗者。</li>
+						<li>專案團隊經驗者或小型工作團隊經驗者。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>資料庫系統管理工程師(DB2、SQL、Open source)</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>資料庫及相關軟硬體建置、維護與監控。</li>
+						<li>資料庫效能分析與優化調校。</li>
+						<li>資訊系統專案架構之規劃、評估與建置。</li>
+					</ul>
+					<br>
+					歡迎具下列任一領域經驗、專長或證照者報名：<br>
+					<ul>
+						<li>AIX、Linux系統相關管理。</li>
+						<li>DB2或SQL Server系統管理。</li>
+						<li>大型資料庫管理及設計經驗者。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>雲端虛擬化系統工程師(VMWare、Hyper-V)</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>規劃、維運雲端虛擬化技術平台，並協助轉移現有系統至雲端平台。</li>
+						<li>評估、導入雲端、開源技術或新資訊科技應用。</li>
+					</ul>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>具VMware、Hyper-V虛擬化技術、Windows、Linux伺服器作業系統或Jboss應用伺服器等任一技術建置、維運有經驗者尤佳，有意願者亦可。</li>
+						<li>對雲端、開源技術或新資訊科技應用評估、規劃與發展有意願者。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>網路系統管理工程師</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>規劃、維運企業網路系統、無線網路、防火牆之建置、維護與監控。</li>
+						<li>網路系統專案架構之規劃、評估與建置。</li>
+						<li>評估、導入網路安全技術或網路新科技應用。</li>
+					</ul>
+					<br>
+					歡迎具下列任一領域經驗、專長或證照者：<br>
+					<ul>
+						<li>熟悉TCP/IP網路架構及規劃(Router/Switch/Wireless)。</li>
+						<li>CCNA證照。</li>
+						<li>Windows、UNIX、Linux等作業系統經驗者。</li>
+						<li>專案團隊經驗者或小型工作團隊經驗者。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>應用系統品質管理工程師</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					負責應用系統迴歸架構、測試自動化及效能監測之規劃、開發與維護。<br>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>歡迎無資訊背景但對軟體測試及程式開發有興趣者。</li>
+						<li>具HTML、JavaScript、Java EE開發經驗者或軟體開發測試證照尤佳。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>大數據資料工程師</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>因應金融業務場景，開發大數據資料蒐集、清洗及邏輯處</li>
+						<li>規劃大數據資料倉儲與應用平台(如：機器學習平台、商業智慧工具)架構。</li>
+					</ul>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>大學日間部以上，數理統計、資工資管等相關等系所畢。</li
+						<li>熟悉資料結構與關聯式資料庫存取語言SQL並具備ETL相關技術。</li>
+						<li>熟悉Hadoop架構與Spark。</li>
+						<li>具備程式撰寫能力(R或Python)。</li>
+						<li>具大數據機器學習相關開發框架經驗者尤佳，如：Caffe、Theano、TensorFlow、Torch。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>前端工程師 (Front-end Developer)</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>與UX/UI及開發人員共同合作，規劃與設計網路投保、會員網站及行銷類網站的</li>
+						<li>網頁程式開發。</li>
+						<li>串接Ajax、Restful API。</li>
+						<li>前端技術研究。</li>
+					</ul>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>對前端開發</li>
+						<li>善於溝通、對改善產品使用者經驗有強烈的興趣與企圖心。</li>
+						<li>熟悉JavaScript、CSS等開發，有相關工作經驗者尤佳。</li>
+						<li>熟悉任一前端框架(Angular, React, Vue)、具RWD網站開發或各式版型模組化能力、具UX/UI經驗、具後端經驗者尤佳。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>智能客服研發工程師</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>運用語音辨識，語意理解等技術於對話機器人，知識庫等客服應用系統之規劃、開發</li>
+						<li>客服資通訊科技之應用研究及導入評估。</li>
+					</ul>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>具Java EE、JavaScript、Node-RED等開發經驗</li>
+						<li>具ChatBot，語音辨識應用之經驗者尤佳。</li>
+					</ul>
+				</div>
+			</div>
+			<div class="job">
+				<h4>iOS/Android App 開發工程師</h4>
+				<h5>國泰人壽</h5>
+				<div class="btn">立即填寫履歷</div>
+				<div class="line"></div>
+				<div class="content">
+					工作說明：<br>
+					<ul>
+						<li>iOS／Android App相關專案之系統開發</li>
+						<li>加入App敏捷團隊，與 UX/UI 及後端開發人員共同合作。</li>
+					</ul>
+					<br>
+					職位要求：<br>
+					<ul>
+						<li>熱愛程式開發，無資訊背</li>
+						<li> 善於溝通、對改善產品使用者經驗有強烈的興趣與企圖心。</li>
+						<li>有 iOS/Android App 開發、測試、上架、性能優化經驗者尤佳，附個人相關作品。</li>
+						<li>有使用 Git 軟體開發經驗。</li>
+						<li>熟悉RESTful規範。</li>
+						<li具下列任一項目者尤佳：</li>
+						<ul>
+							<li>iOS以Swift/Objective-C為主要開發語言，熟CocoPods/Carthage。</li>
+							<li>Android 熟 Kotlin。</li>
+							<li>Design pattern/MVC/Git flow。 </li>
+							<li>Jenkins軟體開發工具經驗。</li>
+							<li>Agile團隊經驗。</li>
+						</ul>
+					</ul>
+				</div>
+			</div>
+		</b-container>
 	</b-container>
-
 </template>
 
 <script>
+import Nav from '@/components/nav';
+
 export default {
 	name: 'Jobs',
-	data() {
-		return {
-			msg: '所有職缺'
-		}
+	components: {
+		Nav
 	}
 }
 </script>
@@ -311,7 +304,6 @@ export default {
 
 	#jobs
 		padding: 0 10vw
-		padding-top: 100px
 		color: $black
 
 		ul
