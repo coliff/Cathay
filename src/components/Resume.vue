a7b19ded4a68b528c4314aba7abe52b27456f476
ShaoEnChen <seanchen47@gmail.com>
2018-09-16 12:32:51 -0500

    add resume submit console error

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -310,7 +310,7 @@ export default {
 				}
 			}).catch((error) => {
 				// err handling
-				console.log(err);
+				console.log(error);
 				if (error.status === '400') {
 					alert('格式有誤，請確認後重新上傳');
 				} else {
