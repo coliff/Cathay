67f75aa108b18942cdf611aee37e19c5f63e07e9
ShaoEnChen <seanchen47@gmail.com>
2018-09-19 18:38:05 -0500

    deal with browser problems

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -7,13 +7,13 @@
 		<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 		<b-collapse is-nav id="nav-collapse">
 			<b-navbar-nav class="mr-auto">
-				<b-nav-item v-if="isIndex" to="/#to-jobs" class="px-2">職缺一覽</b-nav-item>
-				<b-nav-item v-else to="/jobs" class="px-2">職缺一覽</b-nav-item>
-				<b-nav-item to="/#intro" class="px-2">IT's Time 介紹</b-nav-item>
-				<b-nav-item to="/#welfare" class="px-2">集團福利</b-nav-item>
-				<b-nav-item to="/#to-ob" class="px-2">OB分享</b-nav-item>
-				<b-nav-item to="/#schedule" class="px-2">申請流程</b-nav-item>
-				<b-nav-item to="/#interview" class="px-2">面試會流程</b-nav-item>
+				<b-nav-item v-if="isIndex" to="/#to-jobs" class="px-3 px-md-2">職缺一覽</b-nav-item>
+				<b-nav-item v-else to="/jobs" class="px-3 px-md-2">職缺一覽</b-nav-item>
+				<b-nav-item to="/#intro" class="px-3 px-md-2">IT's Time 介紹</b-nav-item>
+				<b-nav-item to="/#welfare" class="px-3 px-md-2">集團福利</b-nav-item>
+				<b-nav-item to="/#to-ob" class="px-3 px-md-2">OB分享</b-nav-item>
+				<b-nav-item to="/#schedule" class="px-3 px-md-2">申請流程</b-nav-item>
+				<b-nav-item to="/#interview" class="px-3 px-md-2">面試會流程</b-nav-item>
 			</b-navbar-nav>
 			<b-navbar-nav>
 				<b-nav-item to="/resume" id="to-resume">履歷投遞</b-nav-item>
@@ -40,10 +40,13 @@ export default {
 	#brand
 		box-sizing: content-box
 		width: 40%
-		img
-			height: auto
+
 		@include media-breakpoint-up(md)
 			width: 20%
+
+	li
+		@include media-breakpoint-only(md)
+			font-size: 1.45vw;
 
 	#to-resume
 		.nav-link
