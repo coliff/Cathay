fc4ffe26a2d29bd66bde16946636ba7fa6c5654f
ShaoEnChen <seanchen47@gmail.com>
2018-09-20 02:32:12 -0500

    remove required field

--- src/components/resume-form/portfolio.vue
+++ src/components/resume-form/portfolio.vue
@@ -4,11 +4,11 @@
 		<p class="content">
 			想讓我們更暸解你嗎？<br>
 			快把個人自傳或作品集上傳吧！<br>
-			（限使用 pdf 檔案，檔案大小限制為 15 MB，點選送出按鈕後才會進行上傳）*
+			（限使用 pdf 檔案，檔案大小限制為 15 MB，點選送出按鈕後才會進行上傳）
 		</p>
 		<b-row class="my-1">
 			<b-col lg="8">
-				<b-form-file required accept=".pdf" id="portfolio" name="resume"></b-form-file>
+				<b-form-file accept=".pdf" id="portfolio" name="resume"></b-form-file>
 				<small class="text-muted">{{ errmsg.file }}</small>
 			</b-col>
 		</b-row>
