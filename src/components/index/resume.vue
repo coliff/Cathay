4e5a2cbfa7bb52860f1b11f366c3bfc0622b83cc
ShaoEnChen <seanchen47@gmail.com>
2018-09-29 17:01:49 -0500

    fix repetitive ids

--- src/components/index/resume.vue
+++ src/components/index/resume.vue
@@ -6,12 +6,12 @@
 					<img src="@/assets/img/resume.png" class="d-block mx-auto" alt="履歷投遞">
 				</b-link>
 				<br>
-				<div id="intro">
+				<div id="remark">
 					【註一】建議可事先備妥客製化自傳 / 履歷 / 作品集，以及個人近照，以節省您履歷填寫的時間<br>
 					【註二】請勿重覆投遞履歷
 				</div>
 				<br><br><br>
-				<div id="contact">
+				<div id="now">
 					優秀的您還在等什麼？<br>
 					國泰提供一個能讓您大顯身手的舞台<br>
 					誠摯邀請懷抱熱情的 IT 人與我們一同大放異彩<br>
@@ -33,14 +33,14 @@ export default {
 		background-color: $black
 		padding: 100px 0px
 
-		#intro
+		#remark
 			text-align: center
 			color: $white
 			font-size: 14px
 			font-weight: bold
 			opacity: 0.5
 
-		#contact
+		#now
 			text-align: center
 			color: $white
 			line-height: 28px
